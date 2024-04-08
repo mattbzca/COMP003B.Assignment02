8a34283489407687e8544b8eeef5311557c5b444
mattbzca <111474919+mattbzca@users.noreply.github.com>
2024-04-07 23:16:57 -0700

    Add project files.

---
+++ COMP003B.Assignment02/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace COMP003B.Assignment02.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
